dc9fa61cecaede5161727d105277f8864b07b08d
Gunnar Hillert <gunnar.hillert@oracle.com>
2021-01-11 08:06:16 -1000

    Polish Javadoc

---
+++ coherence-spring-boot-starter/src/main/java/com/oracle/coherence/spring/boot/autoconfigure/support/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes supporting the {@code com.oracle.coherence.spring.boot.autoconfigure} package.
+ */
+package com.oracle.coherence.spring.boot.autoconfigure.support;
---
+++ coherence-spring-core/src/main/java/com/oracle/coherence/spring/configuration/support/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes supporting the {@code com.oracle.coherence.spring.configuration} package.
+ */
+package com.oracle.coherence.spring.configuration.support;
