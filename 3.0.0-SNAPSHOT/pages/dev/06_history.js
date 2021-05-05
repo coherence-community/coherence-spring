81c1a54c5f36975c41520b37a82adc1aad2af79a
Gunnar Hillert <gunnar@hillert.com>
2021-05-05 22:39:01 +0000

    3.0.0-SNAPSHOT Site

--- 3.0.0-SNAPSHOT/pages/dev/06_history.js
+++ 3.0.0-SNAPSHOT/pages/dev/06_history.js
@@ -6,7 +6,7 @@
 reverse chronological order.</p>
 
 
-<h3 id="_version_3_0_0_snapshot_built_on_2021_05_05_0904">Version 3.0.0-SNAPSHOT built on 2021-05-05 09:04</h3>
+<h3 id="_version_3_0_0_snapshot_built_on_2021_05_05_2231">Version 3.0.0-SNAPSHOT built on 2021-05-05 22:31</h3>
 <div class="section">
 <p><strong>Source and Documentation Contributors</strong></p>
 
