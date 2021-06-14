fcb360533a5506342f0471c94f8c9363230c224c
Gunnar Hillert <gunnar.hillert@oracle.com>
2021-06-13 21:21:10 -1000

    3.0.0-SNAPSHOT Site

---
+++ 3.0.0-SNAPSHOT/pages/about/08_spring-data.js
@@ -0,0 +1,9 @@
+<doc-view>
+
+<h2 id="_spring_data">Spring Data</h2>
+<div class="section">
+<p>The Spring Data integration allows you to use Coherence as a persistence store, providing among other things
+Spring Data repository support.</p>
+
+</div>
+</doc-view>
