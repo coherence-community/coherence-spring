92b000ac094c6a9c905d35385a9a2290e2cf4832
GitHub Action <github-action@users.noreply.github.com>
2022-01-21 00:29:09 +0000

    3.1.0 Site

---
+++ 3.1.0/pages/dev/03_build-instructions.js
@@ -0,0 +1,26 @@
+<doc-view>
+<p>To build the Coherence Spring project you need to have the following software installed:</p>
+
+<ul class="ulist">
+<li>
+<p><a id="" title="" target="_blank" href="http://www.oracle.com/technetwork/java/javase/downloads/index.html">Java SE Development Kit 11</a>.</p>
+
+</li>
+<li>
+<p><a id="" title="" target="_blank" href="http://maven.apache.org/">Apache Maven</a></p>
+
+</li>
+</ul>
+<p>Once you have these installed building the entire source tree is as simple as executing the following shell command
+(from the root of the source directory):</p>
+
+<markup
+lang="bash"
+
+>./mvnw clean package -Pspring</markup>
+
+<p>For detailed instructions, please see the main
+<a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-spring/blob/master/README.adoc">README</a> of the Coherence Spring source
+code repository.</p>
+
+</doc-view>
