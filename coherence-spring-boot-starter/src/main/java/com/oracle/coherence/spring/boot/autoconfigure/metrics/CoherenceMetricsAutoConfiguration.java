473786893ea787a301c0463fdfc76722a177fc9e
Gunnar Hillert <gunnar.hillert@oracle.com>
2022-08-01 13:58:36 -1000

    Migrate to new AutoConfiguration support in Boot 2.7
    - add org.springframework.boot.autoconfigure.AutoConfiguration.imports file
    - use @AutoConfiguration annotation (since Spring Boot 2.7)

--- coherence-spring-boot-starter/src/main/java/com/oracle/coherence/spring/boot/autoconfigure/metrics/CoherenceMetricsAutoConfiguration.java
+++ coherence-spring-boot-starter/src/main/java/com/oracle/coherence/spring/boot/autoconfigure/metrics/CoherenceMetricsAutoConfiguration.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
 package com.oracle.coherence.spring.boot.autoconfigure.metrics;
 
 
@@ -7,20 +13,21 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for {@link CoherenceMicrometerMetrics}.
  *
  * @author Vaso Putica
+ * @author Gunnar Hillert
  * @since 3.0
  */
-@Configuration(proxyBeanMethods = false)
+@AutoConfiguration
 @ConditionalOnMissingBean(CoherenceMicrometerMetrics.class)
 @AutoConfigureAfter({CompositeMeterRegistryAutoConfiguration.class, MetricsAutoConfiguration.class})
 @ConditionalOnClass({CoherenceMicrometerMetrics.class, MeterRegistry.class})
