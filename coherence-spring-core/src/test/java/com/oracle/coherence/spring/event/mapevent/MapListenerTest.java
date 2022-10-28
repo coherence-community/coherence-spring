c47b46e323b9ec9a3076ba8462d15582efbaf493
Gunnar Hillert <gunnar.hillert@oracle.com>
2022-10-27 23:03:14 -1000

    Update Bedrock dependency version from 6.0.0 to 6.0.2

--- coherence-spring-core/src/test/java/com/oracle/coherence/spring/event/mapevent/MapListenerTest.java
+++ coherence-spring-core/src/test/java/com/oracle/coherence/spring/event/mapevent/MapListenerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2013, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -11,28 +11,14 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
 import com.oracle.bedrock.testsupport.deferred.Eventually;
-import com.oracle.coherence.common.collections.ConcurrentHashMap;
 import com.oracle.coherence.spring.annotation.MapEventTransformerBinding;
 import com.oracle.coherence.spring.annotation.MapEventTransformerFactory;
 import com.oracle.coherence.spring.annotation.Name;
-import com.oracle.coherence.spring.annotation.PropertyExtractor;
-import com.oracle.coherence.spring.annotation.WhereFilter;
-import com.oracle.coherence.spring.annotation.event.CacheName;
-import com.oracle.coherence.spring.annotation.event.Deleted;
-import com.oracle.coherence.spring.annotation.event.Inserted;
-import com.oracle.coherence.spring.annotation.event.MapName;
-import com.oracle.coherence.spring.annotation.event.ScopeName;
-import com.oracle.coherence.spring.annotation.event.ServiceName;
-import com.oracle.coherence.spring.annotation.event.Synchronous;
-import com.oracle.coherence.spring.annotation.event.Updated;
 import com.oracle.coherence.spring.configuration.annotation.EnableCoherence;
 import com.oracle.coherence.spring.configuration.session.SessionConfigurationBean;
 import com.oracle.coherence.spring.configuration.session.SessionType;
@@ -43,6 +29,8 @@ import com.tangosol.util.MapEvent;
 import com.tangosol.util.MapEventTransformer;
 import data.Person;
 import data.PhoneNumber;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -66,6 +54,9 @@ import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
 @DirtiesContext
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MapListenerTest {
+
+	private static final Log logger = LogFactory.getLog(MapListenerTest.class);
+
 	@Inject
 	@Name("test")
 	Session session;
@@ -143,78 +134,6 @@ class MapListenerTest {
 			p.setLastName(p.getLastName().toUpperCase());
 			entry.setValue(p);
 			return null;
-		}
-	}
-
-	public static class TestListener {
-		private final Map<Integer, Integer> events = new ConcurrentHashMap<>();
-
-		private final List<MapEvent<String, Person>> filteredEvents = Collections.synchronizedList(new ArrayList<>());
-
-		private final List<MapEvent<String, String>> transformedEvents = Collections.synchronizedList(new ArrayList<>());
-
-		private final List<MapEvent<String, String>> transformedWithExtractorEvents = Collections.synchronizedList(new ArrayList<>());
-
-		Integer getEvents(int id) {
-			return this.events.get(id);
-		}
-
-		public List<MapEvent<String, Person>> getFilteredEvents() {
-			return this.filteredEvents;
-		}
-
-		public List<MapEvent<String, String>> getTransformedEvents() {
-			return this.transformedEvents;
-		}
-
-		public List<MapEvent<String, String>> getTransformedWithExtractorEvents() {
-			return this.transformedWithExtractorEvents;
-		}
-
-		@Synchronous
-		@WhereFilter("firstName = 'Bart' and lastName = 'Simpson'")
-		@CoherenceEventListener
-		void onHomer(@CacheName("people") MapEvent<String, Person> event) {
-			this.filteredEvents.add(event);
-		}
-
-		@Synchronous
-		@CoherenceEventListener
-		void onPersonDeleted(@Deleted @CacheName("people") MapEvent<String, Person> event) {
-			record(event);
-		}
-
-		@Synchronous
-		@CoherenceEventListener
-		void onPersonInserted(@Inserted @ScopeName("Test") @MapName("people") MapEvent<String, Person> event) {
-			record(event);
-			assertThat(event.getNewValue().getLastName(), is("Simpson"));
-		}
-
-		@Synchronous
-		@PropertyExtractor("firstName")
-		@CoherenceEventListener
-		void onPersonInsertedTransformedWithExtractor(@Inserted @MapName("people") MapEvent<String, String> event) {
-			this.transformedWithExtractorEvents.add(event);
-		}
-
-		@Synchronous
-		@UppercaseName
-		@CoherenceEventListener
-		void onPersonInsertedTransformed(@Inserted @MapName("people") MapEvent<String, String> event) {
-			this.transformedEvents.add(event);
-		}
-
-		@Synchronous
-		@CoherenceEventListener
-		void onPersonUpdated(@Updated @ServiceName("StorageService") @MapName("people") MapEvent<String, Person> event) {
-			record(event);
-			assertThat(event.getOldValue().getLastName(), is("Simpson"));
-			assertThat(event.getNewValue().getLastName(), is("SIMPSON"));
-		}
-
-		private void record(MapEvent<String, Person> event) {
-			this.events.compute(event.getId(), (k, v) -> (v != null) ? v + 1 : 1);
 		}
 	}
 
