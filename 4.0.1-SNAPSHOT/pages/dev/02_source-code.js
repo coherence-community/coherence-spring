1da2f445ca379aad3ee3a60df72f3bd28d312a40
GitHub Action <github-action@users.noreply.github.com>
2023-07-05 16:42:56 +0000

    4.0.1-SNAPSHOT Site

---
+++ 4.0.1-SNAPSHOT/pages/dev/02_source-code.js
@@ -0,0 +1,24 @@
+<doc-view>
+
+<h2 id="_source_code">Source Code</h2>
+<div class="section">
+<p>The Source Code for the Oracle Coherence Spring Integration is hosted by <a target="_blank" href="https://github.com">github.com</a>.</p>
+
+<p>It is located here:
+<a target="_blank" href="https://github.com/coherence-community/coherence-spring" class="bare">https://github.com/coherence-community/coherence-spring</a></p>
+
+<p>Alternatively, if you&#8217;re not using or want to avoid setting up <a target="_blank" href="http://git-scm.com">git</a>, you
+can download a zip containing the latest development branch from here:</p>
+
+<p><a target="_blank" href="https://github.com/coherence-community/coherence-spring/archive/main.zip" class="bare">https://github.com/coherence-community/coherence-spring/archive/main.zip</a></p>
+
+<div class="admonition important">
+<p class="admonition-textlabel">Important</p>
+<p ><p><strong>Pull Requests</strong> are welcome! Just make sure to become an official
+<a target="_blank" href="https://github.com/coherence-community/coherence-spring/blob/main/CONTRIBUTING.md">contributor</a>.</p>
+</p>
+</div>
+
+</div>
+
+</doc-view>
