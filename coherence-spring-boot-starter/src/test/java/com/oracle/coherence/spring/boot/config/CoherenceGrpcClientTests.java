5c9d711ad78442f8e096acc13b0056d4f7b71564
Gunnar Hillert <gunnar.hillert@oracle.com>
2023-09-13 11:14:38 -1000

    Convert CoherenceGrpcClientTests to use Bedrock

--- coherence-spring-boot-starter/src/test/java/com/oracle/coherence/spring/boot/config/CoherenceGrpcClientTests.java
+++ coherence-spring-boot-starter/src/test/java/com/oracle/coherence/spring/boot/config/CoherenceGrpcClientTests.java
@@ -1,17 +1,26 @@
 /*
- * Copyright (c) 2013, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2013, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
  */
 package com.oracle.coherence.spring.boot.config;
 
-import com.oracle.coherence.spring.test.junit.CoherenceServerJunitExtension;
-import com.tangosol.net.Coherence;
+import java.util.concurrent.TimeUnit;
+
+import com.oracle.bedrock.runtime.LocalPlatform;
+import com.oracle.bedrock.runtime.coherence.CoherenceClusterMember;
+import com.oracle.bedrock.runtime.coherence.options.LocalHost;
+import com.oracle.bedrock.runtime.java.options.IPv4Preferred;
+import com.oracle.bedrock.runtime.java.options.SystemProperty;
+import com.oracle.bedrock.runtime.options.DisplayName;
+import com.oracle.coherence.spring.test.utils.NetworkUtils;
 import com.tangosol.net.NamedCache;
 import com.tangosol.net.Session;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,18 +31,33 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class CoherenceGrpcClientTests {
 
-	@RegisterExtension
-	static CoherenceServerJunitExtension coherenceServerJunitExtension =
-			new CoherenceServerJunitExtension(true);
+	static CoherenceClusterMember server;
 
-	final Coherence coherence;
+	@BeforeAll
+	static void setup() throws Exception {
+		final LocalPlatform platform = LocalPlatform.get();
 
-	public CoherenceGrpcClientTests(Coherence coherence) {
-		this.coherence = coherence;
+		// Start the Coherence server
+		server = platform.launch(CoherenceClusterMember.class,
+				LocalHost.only(),
+				IPv4Preferred.yes(),
+				SystemProperty.of("coherence.cluster", "CoherenceGrpcClientTestsCluster"),
+				SystemProperty.of("coherence.grpc.enabled", true),
+				SystemProperty.of("coherence.grpc.server.port", "1408"),
+				DisplayName.of("server"));
+
+		Awaitility.await().atMost(70, TimeUnit.SECONDS).until(() -> NetworkUtils.isGrpcPortInUse());
+	}
+
+	@AfterAll
+	static void cleanup() {
+		if (server != null) {
+			server.close();
+		}
 	}
 
 	@Test
-	public void testDefaultCacheManagerExists() throws Exception {
+	public void testCoherenceGrpcClient() throws Exception {
 		final CoherenceConfigClientProperties coherenceConfigClientProperties = new CoherenceConfigClientProperties();
 		coherenceConfigClientProperties.getClient().setHost("localhost");
 		coherenceConfigClientProperties.getClient().setPort(1408);
@@ -46,7 +70,7 @@ public class CoherenceGrpcClientTests {
 
 		coherenceGrpcClient.close();
 
-		final Session coherenceSession = this.coherence.getSession();
+		final Session coherenceSession = server.getSession();
 		assertThat(coherenceSession.getCache("test")).hasSize(1);
 		assertThat(coherenceSession.getCache("test").get("foo2")).isEqualTo("bar2");
 	}
