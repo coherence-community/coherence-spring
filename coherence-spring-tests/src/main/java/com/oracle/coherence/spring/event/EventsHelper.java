512b87b5344a0a8fb79903e498b51a5d5cca345f
Gunnar Hillert <gunnar.hillert@oracle.com>
2023-10-17 12:26:18 -1000

    Upgrade Coherence versions to 22.06.6
    - Upgrade Assertj version to 3.24.2
    - Upgrade Awaitility version to 4.2.0
    - Upgrade Classgraph version to 4.8.163
    - Upgrade Log4j version to 2.21.0
    - Upgrade Spring Framework version to 5.3.30
    - Upgrade Spring Data version to 2021.2.17
    - Upgrade Reactor version to 3.4.33
    - Upgrade Spring Boot version to 2.7.16
    - Upgrade Spring Security version to 5.7.11
    - Upgrade Tomcat version to 9.0.82

--- coherence-spring-tests/src/main/java/com/oracle/coherence/spring/event/EventsHelper.java
+++ coherence-spring-tests/src/main/java/com/oracle/coherence/spring/event/EventsHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2013, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -10,6 +10,7 @@ package com.oracle.coherence.spring.event;
 import java.util.Map;
 
 import com.tangosol.coherence.component.util.daemon.queueProcessor.service.grid.partitionedService.PartitionedCache;
+import com.tangosol.coherence.component.util.daemon.queueProcessor.service.grid.partitionedService.partitionedCache.Storage;
 import com.tangosol.coherence.component.util.safeService.SafeCacheService;
 import com.tangosol.net.CacheService;
 import com.tangosol.net.NamedMap;
@@ -29,21 +30,21 @@ public final class EventsHelper {
 	}
 
 	public static int getTotalListenerCount(NamedMap<?, ?> map) {
-		PartitionedCache.Storage storage = getStorage(map);
+		Storage storage = getStorage(map);
 		return getKeyListenerCount(storage) + getListenerCount(storage);
 	}
 
 	public static int getListenerCount(NamedMap<?, ?> map) {
-		PartitionedCache.Storage storage = getStorage(map);
+		Storage storage = getStorage(map);
 		return getListenerCount(storage);
 	}
 
 	public static int getKeyListenerCount(NamedMap<?, ?> map) {
-		PartitionedCache.Storage storage = getStorage(map);
+		Storage storage = getStorage(map);
 		return getKeyListenerCount(storage);
 	}
 
-	private static int getListenerCount(PartitionedCache.Storage storage) {
+	private static int getListenerCount(Storage storage) {
 		if (storage == null) {
 			return 0;
 		}
@@ -51,7 +52,7 @@ public final class EventsHelper {
 		return (map != null) ? map.size() : 0;
 	}
 
-	private static int getKeyListenerCount(PartitionedCache.Storage storage) {
+	private static int getKeyListenerCount(Storage storage) {
 		if (storage == null) {
 			return 0;
 		}
@@ -59,7 +60,7 @@ public final class EventsHelper {
 		return (map != null) ? map.size() : 0;
 	}
 
-	private static PartitionedCache.Storage getStorage(NamedMap<?, ?> map) {
+	private static Storage getStorage(NamedMap<?, ?> map) {
 		CacheService service = map.getService();
 		if (service instanceof SafeCacheService) {
 			service = ((SafeCacheService) service).getRunningCacheService();
