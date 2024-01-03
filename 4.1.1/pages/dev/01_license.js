7e9f28f18fd9e7d01248dfb32ea3fbd0c75b5213
GitHub Action <github-action@users.noreply.github.com>
2024-01-03 02:04:54 +0000

    4.1.1 Site

---
+++ 4.1.1/pages/dev/01_license.js
@@ -0,0 +1,47 @@
+<doc-view>
+
+<h2 id="_license">License</h2>
+<div class="section">
+<p>Oracle licenses the Oracle Coherence Spring project under the
+<a target="_blank" href="https://oss.oracle.com/licenses/upl/">The Universal Permissive License (UPL), Version 1.0</a></p>
+
+
+<h3 id="_the_universal_permissive_license_upl_version_1_0">The Universal Permissive License (UPL), Version 1.0</h3>
+<div class="section">
+<p>Subject to the condition set forth below, permission is hereby granted to any
+person obtaining a copy of this software, associated documentation and/or data
+(collectively the "Software"), free of charge and under any and all copyright
+rights in the Software, and any and all patent rights owned or freely
+licensable by each licensor hereunder covering either (i) the unmodified
+Software as contributed to or provided by such licensor, or (ii) the Larger
+Works (as defined below), to deal in both</p>
+
+<p>(a) the Software, and
+(b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+one is included with the Software (each a "Larger Work" to which the Software
+is contributed by such licensors),</p>
+
+<p>without restriction, including without limitation the rights to copy, create
+derivative works of, display, perform, and distribute the Software and make,
+use, sell, offer for sale, import, export, have made, and have sold the
+Software and the Larger Work(s), and to sublicense the foregoing rights on
+either these or other terms.</p>
+
+<p>This license is subject to the following condition:
+The above copyright notice and either this complete permission notice or at
+a minimum a reference to the UPL must be included in all copies or
+substantial portions of the Software.</p>
+
+<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.</p>
+
+</div>
+
+</div>
+
+</doc-view>
---
+++ 4.1.1/pages/dev/03_build-instructions.js
@@ -0,0 +1,27 @@
+<doc-view>
+<p>To build the Coherence Spring project you need to have the following software installed:</p>
+
+<ul class="ulist">
+<li>
+<p><a target="_blank" href="http://www.oracle.com/technetwork/java/javase/downloads/index.html">Java SE Development Kit 17</a>.</p>
+
+</li>
+<li>
+<p><a target="_blank" href="http://maven.apache.org/">Apache Maven</a></p>
+
+</li>
+</ul>
+
+<p>Once you have these installed building the entire source tree is as simple as executing the following shell command
+(from the root of the source directory):</p>
+
+<markup
+lang="bash"
+
+>./mvnw clean package</markup>
+
+<p>For detailed instructions, please see the main
+<a target="_blank" href="https://github.com/coherence-community/coherence-spring/blob/main/README.adoc">README</a> of the Coherence Spring source
+code repository.</p>
+
+</doc-view>
