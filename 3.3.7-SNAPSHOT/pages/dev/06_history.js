c8bce3a44eb4cd85aa50d3a8412c41098615f29e
GitHub Action <github-action@users.noreply.github.com>
2024-07-03 08:27:53 +0000

    3.3.7-SNAPSHOT Site

--- 3.3.7-SNAPSHOT/pages/dev/06_history.js
+++ 3.3.7-SNAPSHOT/pages/dev/06_history.js
@@ -6,6 +6,12 @@
 reverse chronological order.</p>
 
 
+<h3 id="_version_3_4_0_july_2024">Version 3.4.0 (July 2024)</h3>
+<div class="section">
+<p><a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-spring/releases/tag/v3.4.0">https://github.com/coherence-community/coherence-spring/releases/tag/v3.4.0</a></p>
+
+</div>
+
 <h3 id="_version_3_3_6_january_2024">Version 3.3.6 (January 2024)</h3>
 <div class="section">
 <p><a id="" title="" target="_blank" href="https://github.com/coherence-community/coherence-spring/releases/tag/v3.3.6">https://github.com/coherence-community/coherence-spring/releases/tag/v3.3.6</a></p>
